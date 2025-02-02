f3221a51623f9bd405564a822debc1f68ff6b434
FillArt <artiom.filipopovschii@gmail.com>
2025-02-01 22:38:11 -0400

    feat: add props for title in TodoListItem components

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,12 @@
 import './App.css'
 import {TodoListItem} from "./TodoListItem.tsx";
 
-function App() {
+export const  App = () => {
     return (
         <div className="app">
-            <TodoListItem/>
-            <TodoListItem/>
-            <TodoListItem/>
+            <TodoListItem title={"First To-Do"} />
+            <TodoListItem title={"Second To-Do"} />
+            <TodoListItem title={"Why not To-Do"} />
         </div>
     )
 }
-
-export default App
--- src/TodoListItem.tsx
+++ src/TodoListItem.tsx
@@ -1,7 +1,11 @@
-export const TodoListItem = () => {
+type TodoListProps = {
+    title: string
+}
+
+export const TodoListItem = ({title}: TodoListProps) => {
     return (
         <div>
-            <h3>What to learn</h3>
+            <h3>{title}</h3>
             <div>
                 <input/>
                 <button>+</button>
