32e355f5305a207173e0867c1128d297f341fef1
FillArt <artiom.filipopovschii@gmail.com>
2025-02-05 11:58:04 -0400

    feat: added the logic that if focus is lost, the editing mode is disabled.

--- src/EditableSpan.tsx
+++ src/EditableSpan.tsx
@@ -9,11 +9,12 @@ export const EditableSpan = ({value}: Props) => {
     const [isEditMode, setIsEditMode] = useState<boolean>(false)
 
     const turnOnEditMode = () => {setIsEditMode(true)}
+    const turnOffEditMode = () => {setIsEditMode(false)}
 
     return (
             <>
                 {isEditMode ? (
-                    <input type="text" value={value} autoFocus />
+                    <input type="text" value={value} autoFocus onBlur={turnOffEditMode} />
                 ) : (
                     <span onDoubleClick={turnOnEditMode}>{value}</span>
                 )}
