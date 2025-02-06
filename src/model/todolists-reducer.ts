002829523548c770604db34da05932f81284731c
FillArt <artiom.filipopovschii@gmail.com>
2025-02-05 21:15:02 -0400

    feat: created an action creator function to create a todo list, covered it with tests and typified it.

--- src/model/todolists-reducer.ts
+++ src/model/todolists-reducer.ts
@@ -1,4 +1,5 @@
 import {Todolist} from "../App.tsx";
+import {v1} from "uuid";
 
 const initialState: Todolist[] = []
 
@@ -6,6 +7,11 @@ export const todolistReducer = (state: Todolist[] = initialState, action: Action
     switch (action.type) {
         case 'delete_todolist':
             return state.filter(todo => todo.id !== action.payload.id)
+        case 'create_todolist': {
+            const newTodo: Todolist = {id: action.payload.id, title: action.payload.title, filter: 'all'}
+            return [...state, newTodo]
+        }
+
         default:
             return state
     }
@@ -15,4 +21,12 @@ export const deleteTodolistAC = (id: string) => {
     return {type: 'delete_todolist', payload: { id }} as const
 }
 
+export const createTodolistAC = (title: string) => {
+    return {type: 'create_todolist', payload: { id: v1(), title }} as const
+}
+
+
 export type DeleteTodolistAction = ReturnType<typeof deleteTodolistAC>
+export type CreateTodolistAction = ReturnType<typeof createTodolistAC>
+
+type Actions = DeleteTodolistAction | CreateTodolistAction
